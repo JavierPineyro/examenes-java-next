e392fe164b81ca4c1ed8cb7bad17ecdadc708e41
JavierPineyro <javidunam22@gmail.com>
2023-11-26 22:26:19 -0300

    Add modal form to delete category

--- src/components/Buttons/admin-action-button.jsx
+++ src/components/Buttons/admin-action-button.jsx
@@ -1,16 +1,19 @@
 import { deleteCategory } from '@/lib/actions'
 import { ROL } from '@/lib/utils'
+import ModalDelete from '../Modal/delete-modal'
 
 export default function ActionButtons({ role, token, categoryId }) {
   const deleteCategoryWithId = deleteCategory.bind(null, { id: categoryId, token })
+
+  // <form action={deleteCategoryWithId}>
+  //   <button type='submit' className='px-2 w-40 py-3 rounded-md text-center bg-red-400'>Eliminar</button>
+  // </form>
   return (
     <>
       {
         role === ROL.ADMIN && (
           <>
-            <form action={deleteCategoryWithId}>
-              <button type='submit' className='px-2 w-40 py-3 rounded-md text-center bg-red-400'>Eliminar</button>
-            </form>
+            <ModalDelete deleteAction={deleteCategoryWithId} />
             <button type='submit' className='px-2 w-40 py-3 rounded-md text-center bg-amber-600'>Editar</button>
           </>
         )
---
+++ src/components/Modal/delete-modal.jsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Button, Modal } from 'flowbite-react'
+import { useState } from 'react'
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
+
+export default function ModalDelete({ deleteAction }) {
+  const [openModal, setOpenModal] = useState(false)
+
+  return (
+    <>
+      <Button onClick={() => setOpenModal(true)}>Eliminar</Button>
+      <Modal show={openModal} size='md' onClose={() => setOpenModal(false)} popup>
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200' />
+            <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
+              ¿Quiere eliminar esta categoría?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <form action={deleteAction}>
+                <Button color='failure' type='submit'>
+                  Si, estoy seguro
+                </Button>
+              </form>
+              <Button color='gray' onClick={() => setOpenModal(false)}>
+                No, cancelar
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
