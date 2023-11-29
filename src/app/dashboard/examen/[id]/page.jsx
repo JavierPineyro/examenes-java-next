a7898705a0b65f83b32fd43b1d59d1b59e81dc7a
JavierPineyro <javidunam22@gmail.com>
2023-11-28 22:03:37 -0300

    Add question mock data to try styles

--- src/app/dashboard/examen/[id]/page.jsx
+++ src/app/dashboard/examen/[id]/page.jsx
@@ -11,25 +11,27 @@ export default async function ExamenDetailPage({ params }) {
 
   const exam = await api.exam.getExamenById({ token, id })
 
-  // give array mock data for questions items with title, question1, questio2, question3, question4 and answwer
   const questions = [
     {
-      title: 'What is the capital of France?',
-      question1: 'Paris',
-      question2: 'London',
-      question3: 'Berlin',
-      question4: 'Madrid',
-      answer: 'Paris'
+      id: 1,
+      contenido: '¿Cuál es la capital de Colombia?',
+      opcion1: 'Bogota',
+      opcion2: 'Medellin',
+      opcion3: 'Cali',
+      opcion4: 'Barranquilla',
+      respuesta: 'Bogota'
     },
     {
-      title: 'What is the largest planet in our solar system?',
-      question1: 'Mars',
-      question2: 'Jupiter',
-      question3: 'Saturn',
-      question4: 'Uranus',
-      answer: 'Jupiter'
+      id: 2,
+      contenido: '¿Cuál es la capital de Ecuador?',
+      opcion1: 'Quito',
+      opcion2: 'Guayaquil',
+      opcion3: 'Cuenca',
+      opcion4: 'Loja',
+      respuesta: 'Quito'
     }
   ]
+
   return (
     <Main className='w-full p-2 '>
       <div className='flex justify-between px-4'>
@@ -40,20 +42,20 @@ export default async function ExamenDetailPage({ params }) {
         {
           questions.map((question, index) => {
             return (
-              <div key={question.title + index} className='p-2 shadow-md rounded-md bg-gray-200/60'>
+              <div key={question.id} className='p-2 shadow-md rounded-md bg-gray-200/60'>
                 <header className='flex text-2xl items-center gap-2'>
                   <span className='font-bold pr-4'>{index + 1})</span>
-                  <span>{question.title}</span>
+                  <span>{question.contenido}</span>
                 </header>
                 <div className='columns-2 p-5 tex-lg'>
-                  <p className='mb-1'>A)<span className='font-bold'> {question.question1}</span></p>
-                  <p className='mb-1'>B)<span className='font-bold'> {question.question2}</span></p>
-                  <p className='mb-1'>C)<span className='font-bold'> {question.question3}</span></p>
-                  <p className='mb-1'>D)<span className='font-bold'> {question.question4}</span></p>
+                  <p className='mb-1'>A)<span className='font-bold'> {question.opcion1}</span></p>
+                  <p className='mb-1'>B)<span className='font-bold'> {question.opcion2}</span></p>
+                  <p className='mb-1'>C)<span className='font-bold'> {question.opcion3}</span></p>
+                  <p className='mb-1'>D)<span className='font-bold'> {question.opcion4}</span></p>
                 </div>
                 <hr className='h-px my-3 bg-gray-300 border-0 dark:bg-gray-700' />
                 <div className='flex items-center w-full '>
-                  <p className='font-bold grow text-lg'>Respuesta: {question.answer}</p>
+                  <p className='font-bold grow text-lg'>Respuesta: {question.respuesta}</p>
                   <div className='flex gap-1 pr-5'>
                     <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Eliminar</button>
                     <button className='bg-green-500 text-white px-4 py-2 rounded-md'>Actualizar</button>
